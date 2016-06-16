58424a0055f484133255c9098ef17c780e042cf4
Jose <jabv_1986@telefonica.net>
2016-06-16 11:48:45 +0200

    código reducido

--- scripts/app.js
+++ scripts/app.js
@@ -2,21 +2,7 @@ function alineacion ($scope,$http){
 	var ENDPOINT = "./data/partido.json";
 	$http.get(ENDPOINT).then(function(res){
 		$scope.finales=res.data;
-		console.log($scope.finales);
-		
-		var finales = $scope.finales;
-		console.log(finales[0].copa);
-		$scope.title = "Selecciona la final";
-   $scope.primera=finales[0].copa;
-   $scope.segunda=finales[1].copa;
-   $scope.tercera=finales[2].copa;
-   		
-
-   	$scope.equipos=finales.forEach(function(copa){
-   		console.log(copa.equipos);
-   	});
-   
-    
+		$scope.title = "Selecciona la final";    
 	});
 };
 
